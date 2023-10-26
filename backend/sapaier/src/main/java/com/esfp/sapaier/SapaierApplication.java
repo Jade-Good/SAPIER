d97074c236c5200814027f658dee09053aecbb4b
tjdfkr011 <tjdfkr011@naver.com>
2023-10-26 10:53:06 +0900

    init : add backend server project

---
+++ backend/sapaier/src/main/java/com/esfp/sapaier/SapaierApplication.java
@@ -0,0 +1,13 @@
+package com.esfp.sapaier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapaierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapaierApplication.class, args);
+	}
+
+}
---
+++ backend/sapaier/src/test/java/com/esfp/sapaier/SapaierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esfp.sapaier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapaierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
