13b037d106e39289c9aaffff4587e69e5da3b4bb
tjdfkr011 <tjdfkr011@naver.com>
2023-10-26 10:48:37 +0900

    init : add backend server project

---
+++ backend/sapier/src/test/java/com/esfp/sapier/SapierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esfp.sapier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
