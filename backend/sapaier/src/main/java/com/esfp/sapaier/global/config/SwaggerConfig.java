4250b4190b9ff2691f5de85500ea8346139fc828
tjdfkr011 <tjdfkr011@naver.com>
2023-10-26 13:56:11 +0900

    init : add backend server project

---
+++ backend/sapaier/src/main/java/com/esfp/sapaier/global/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.esfp.sapaier.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+@OpenAPIDefinition(servers = {@Server(url = "https://j9b310.p.ssafy.io", description = "Default Server Url")})
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.components(new Components())
+			.info(apiInfo());
+	}
+
+	private Info apiInfo() {
+		return new Info()
+			.title("Springdoc 테스트")
+			.description("Springdoc을 사용한 Swagger UI 테스트")
+			.version("1.0.0");
+	}
+}
