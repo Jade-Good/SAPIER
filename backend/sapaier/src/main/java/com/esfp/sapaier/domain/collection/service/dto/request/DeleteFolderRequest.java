2056d2d9a744af7ba3c352cba42434d49112d0b9
kyeongin- <a27313797@gmail.com>
2023-10-31 16:12:59 +0900

    Feat/BE : request dto 생성
    - folder 생성 시 사용되는 create folder request
    - folder 삭제 시 사용되는 delete folder request
    - folder 수정 시 사용되는 modify folder request

---
+++ backend/sapaier/src/main/java/com/esfp/sapaier/domain/collection/service/dto/request/DeleteFolderRequest.java
@@ -0,0 +1,10 @@
+package com.esfp.sapaier.domain.collection.service.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DeleteFolderRequest {
+    private String path;
+}
