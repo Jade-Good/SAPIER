3724560d62ed2ec1cc11c93598b9ef83c95e24aa
kyeongin- <a27313797@gmail.com>
2023-11-01 09:23:13 +0900

    Feat/BE : CollectionController 작성
    - 사용안하는 controller 삭제
    - 수정 controller 생성
    - 생성 controller 생성

--- backend/sapaier/src/main/java/com/esfp/sapaier/domain/collection/controller/CollectionController.java
+++ backend/sapaier/src/main/java/com/esfp/sapaier/domain/collection/controller/CollectionController.java
@@ -1,14 +1,12 @@
 package com.esfp.sapaier.domain.collection.controller;
 
 import com.esfp.sapaier.domain.collection.service.CollectionService;
-import com.esfp.sapaier.domain.collection.service.dto.request.CreateFolderRequest;
-import com.esfp.sapaier.domain.collection.service.dto.request.DeleteFolderRequest;
-import com.esfp.sapaier.domain.collection.service.dto.request.ModifyFolderRequest;
+import com.esfp.sapaier.domain.collection.service.dto.request.CreateCollectionRequest;
+import com.esfp.sapaier.domain.collection.service.dto.request.ModifyCollectionRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -20,43 +18,20 @@ public class CollectionController {
     private final CollectionService collectionService;
 
     @PostMapping
-    public ResponseEntity<Object> registCollection(Authentication authentication){
-      //  log.info("현재 로그인한 사용자 정보로 뭐가 넘어오나? : {}",authentication.getName());
-        collectionService.registCollection();
+    public ResponseEntity<Object> registCollectionDocument(@RequestBody CreateCollectionRequest createCollectionRequest){
+        collectionService.createCollectionDocument(createCollectionRequest);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping("/folder")
-    public ResponseEntity<?> registFolder(@RequestBody CreateFolderRequest createFolderRequest, Authentication authentication) {
-        collectionService.registFolder(createFolderRequest);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @DeleteMapping("/folder")
-    public ResponseEntity<?> deleteFolder(@RequestBody DeleteFolderRequest deleteFolderRequest, Authentication authentication){
+    @GetMapping
+    public ResponseEntity<?> collectionList() {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PatchMapping("/folder")
-    public ResponseEntity<?> modifyFolder(@RequestBody ModifyFolderRequest modifyFolderRequest, Authentication authentication){
-
+   @PatchMapping
+    public ResponseEntity<Object> modifyCollection(@RequestBody ModifyCollectionRequest modifyCollectionRequest){
+        collectionService.modifyCollection(modifyCollectionRequest);
         return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PatchMapping("/{collectionIdx}")
-    public ResponseEntity<?> modifyCollection(){
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{collectionIdx}")
-    public ResponseEntity<?> deleteCollection(){
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PostMapping("/request")
-    public ResponseEntity<?> test(){
-        collectionService.test();
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+   }
 }
