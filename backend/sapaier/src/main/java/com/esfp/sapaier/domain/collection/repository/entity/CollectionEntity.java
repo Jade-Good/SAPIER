72b8f14bcc7bd8f91b8c58a63fd2c953b35b7763
kyeongin- <a27313797@gmail.com>
2023-11-01 09:26:14 +0900

    Refactor/BE : CollectionEntity 수정
    - 변경된 api에 맞춰 entity 수정

--- backend/sapaier/src/main/java/com/esfp/sapaier/domain/collection/repository/entity/CollectionEntity.java
+++ backend/sapaier/src/main/java/com/esfp/sapaier/domain/collection/repository/entity/CollectionEntity.java
@@ -1,33 +1,25 @@
 package com.esfp.sapaier.domain.collection.repository.entity;
 
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-import com.esfp.sapaier.domain.user.repository.entity.User;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-// @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Builder
-@Setter /////////////////////////////////////////////////////////////////
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@AllArgsConstructor
 @Document(collection = "Collection")
 public class CollectionEntity {
 
-	@Id
+	@MongoId
 	private String collectionId;
-	private String path;
-	private String name;
-	private User user;
+	private String collectionName;
 
 	private List<RequestEntity> apiList;
 	private List<CollectionEntity> collectionList;
@@ -37,44 +29,17 @@ public class CollectionEntity {
 	@LastModifiedDate
 	private LocalDateTime modifiedTime;
 
-	/////////////////////////////////////////////////////////////////
 	@Builder
-	public CollectionEntity() {
-
-	}
-	@Builder
-	public CollectionEntity(List<RequestEntity> list) {
-		this.apiList = list;
-	}
-	/////////////////////////////////////////////////////////////////
-
-	@Builder
-	public CollectionEntity(String path) {
-		this.path = path;
-	}
-
-	@Builder
-	public CollectionEntity(String path, String name) {   //, User user){
-		this.path = path;
-		//        this.user = user;
-		this.name = name;
-	}
-
-	public void update(String name) {
-		this.name = name;
-	}
-
-	public void updatePath(String path) {
-		this.path = path;
+	public CollectionEntity(String collectionName , List<CollectionEntity> collectionList) {
+		this.collectionName = collectionName;
+		this.collectionList = collectionList;
 	}
 
 	@Override
 	public String toString() {
 		return "CollectionEntity{" +
 			"collectionId='" + collectionId + '\'' +
-			", path='" + path + '\'' +
-			", name='" + name + '\'' +
-			", user=" + user +
+			", collectionName='" + collectionName + '\'' +
 			", apiList=" + apiList +
 			", collectionList=" + collectionList +
 			", createdTime=" + createdTime +
