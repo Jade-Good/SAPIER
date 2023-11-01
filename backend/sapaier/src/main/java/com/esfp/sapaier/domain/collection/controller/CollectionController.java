6807572ef7925e532ef143469eb329e95922625e
kyeongin- <a27313797@gmail.com>
2023-11-01 10:13:30 +0900

    Feat/BE : Collection 조회 controller 작성

--- backend/sapaier/src/main/java/com/esfp/sapaier/domain/collection/controller/CollectionController.java
+++ backend/sapaier/src/main/java/com/esfp/sapaier/domain/collection/controller/CollectionController.java
@@ -1,13 +1,17 @@
 package com.esfp.sapaier.domain.collection.controller;
 
 import com.esfp.sapaier.domain.collection.service.CollectionService;
+import com.esfp.sapaier.domain.collection.service.dto.request.CollectionListRequest;
 import com.esfp.sapaier.domain.collection.service.dto.request.CreateCollectionRequest;
 import com.esfp.sapaier.domain.collection.service.dto.request.ModifyCollectionRequest;
+import com.esfp.sapaier.domain.collection.service.dto.response.CollectionResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -24,9 +28,8 @@ public class CollectionController {
     }
 
     @GetMapping
-    public ResponseEntity<?> collectionList() {
-
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<List<CollectionResponse>> collectionList(@RequestBody CollectionListRequest collectionListRequest) {
+        return new ResponseEntity<>(collectionService.allCollectionList(collectionListRequest),HttpStatus.OK);
     }
 
    @PatchMapping
