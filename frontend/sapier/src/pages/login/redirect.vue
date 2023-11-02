01bfee860e322b97746f410e516940be02c880d8
tjdfkr011 <tjdfkr011@naver.com>
2023-11-02 10:43:56 +0900

    feat : 로그인 리다이렉트 화면 css 완성

--- frontend/sapier/src/pages/login/redirect.vue
+++ frontend/sapier/src/pages/login/redirect.vue
@@ -1,5 +1,18 @@
 <template>
-  <SocialLoginRedirect />
-  <RouterView />
-  <TheFooter />
+  <div class="social_login_page">
+    <SocialLoginContainer/>
+    <SocialLoginRedirect />
+  </div>
 </template>
+
+<style>
+
+.social_login_page {
+   width: 100%;
+   height: 100%;
+   background-color: #0F4C81;
+   overflow: hidden;
+}
+
+
+</style>
