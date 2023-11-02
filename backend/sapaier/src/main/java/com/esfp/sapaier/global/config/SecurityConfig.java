602928b562832412816f0e3a7ab213a308ade1a7
김재원 <kjwkjw1104@gmail.com>
2023-11-02 14:53:37 +0900

    Chore : Securirty Debug Mode Enable

--- backend/sapaier/src/main/java/com/esfp/sapaier/global/config/SecurityConfig.java
+++ backend/sapaier/src/main/java/com/esfp/sapaier/global/config/SecurityConfig.java
@@ -25,7 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
-// @EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
