a7d3f13a153086c483a976e969922eb2886dbee6
ksy <gkstjeo17@naver.com>
2023-11-02 14:24:48 +0900

    Feat/FE : Setting, Overview Component 생성

---
+++ frontend/sapier/src/components/workspace/OverviewComponent.vue
@@ -0,0 +1,150 @@
+<script>
+import axios from 'axios'
+
+const userInfo = useUserStore()
+const isMounted = useMounted()
+
+axios.defaults.withCredentials = true
+
+if (isMounted) {
+  axios
+    .get(`${import.meta.env.VITE_SERVER_URL}/api/v1/workspaces/members`)
+    .then((res) => {
+      console.log(res)
+      userInfo.user = res.data
+    })
+    .catch((error) => {
+      console.log(error)
+    },
+    )
+}
+</script>
+
+<template>
+  <div class="mainInfo">
+    <div class="overview-group">
+      <!-- overview  document  pinned  collection -->
+      <div class="maindiv">
+        <h5 class="maindivHeader">
+          Summmary
+        </h5>
+        <p class="maindivText">
+          개인 워크스페이스 Overview 입니다.
+        </p>
+        <p class="maindivText">
+          워크스페이스 요약, 설명이나 컬렉션을 고정할 수 있습니다.
+        </p>
+        <p class="maindivText">
+          요약, 설명은 클릭하면 수정이 되고, 다른곳을 누르면 저장됩니다.
+          글자수(높이)가 제한되어 있습니다.
+        </p>
+        <p class="maindivText">
+          setting에서는 기타 워크스페이스 설정이나 삭제 및 탈퇴 할 수 있습니다.
+        </p>
+      </div>
+
+      <div class="maindiv">
+        <div class="OC_maindiv_add">
+          <h5 class="maindivHeader">
+            Pinned Collections
+          </h5>
+          <img src="/add.png" class="pin_add_image">
+        </div>
+        <div class="maindivText">
+          sdf
+        </div>
+      </div>
+    </div>
+
+    <div class="document-group">
+      <div class="maindiv">
+        <h5 class="maindivHeader">
+          document
+        </h5>
+        <p class="maindivText">
+          개인 워크스페이스 Overview 입니다.
+        </p>
+        <p class="maindivText">
+          워크스페이스 요약, 설명이나 컬렉션을 고정할 수 있습니다.
+        </p>
+        <p class="maindivText">
+          요약, 설명은 클릭하면 수정이 되고, 다른곳을 누르면 저장됩니다.
+          글자수(높이)가 제한되어 있습니다.
+        </p>
+        <p class="maindivText">
+          setting에서는 기타 워크스페이스 설정이나 삭제 및 탈퇴 할 수 있습니다.
+        </p>
+      </div>
+
+      <div class="maindiv">
+        <h5 class="maindivHeader">
+          Contributors
+        </h5>
+        <div class="maindivText">
+          sdf
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.mainInfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  width: 1220px;
+}
+
+.overview-group {
+  display: flex;
+  margin-bottom: 2em; /*그룹 간의 간격 조절*/
+  width: max-content;
+  gap: 10em;
+  margin-top:2em;
+  margin-left: 13em;
+}
+
+.document-group {
+  display: flex;
+  gap: 10em;
+  margin-left: 13em;
+
+}
+
+.maindiv {
+  display: block;
+  width: 30em;
+  padding-top: 1.5em;
+
+}
+.OC_maindiv_add{
+  display: flex;
+  gap:1em;
+}
+.pin_add_image{
+  width: auto; /* 이미지의 원본 크기를 유지 */
+  max-height: 1.2em; /* 이미지의 최대 높이 설정 */
+
+}
+
+.maindivHeader {
+  display: inline-block;
+  font-size: 1.5em;
+  font-weight: 700;
+}
+
+/* .ContributorsHeader {
+  display: inline-block;
+  font-size: 24px;
+  font-weight: 700;
+} */
+
+.maindivText {
+  font-size: 1em;
+  font-weight: 400;
+  margin-left: 0.5em;
+}
+</style>
--- frontend/sapier/src/components/workspace/WorkSpaceInfo.vue
+++ frontend/sapier/src/components/workspace/WorkSpaceInfo.vue
@@ -1,8 +1,79 @@
 <script>
+import OverviewComponent from './OverviewComponent.vue'
+import SettingComponent from './SettingComponent.vue'
+
+export default {
+  components: {
+    OverviewComponent,
+    SettingComponent,
+  },
+  data() {
+    return {
+      currentComponent: OverviewComponent,
+    }
+  },
+  methods: {
+    showOverviewComponent() {
+      this.currentComponent = OverviewComponent
+    },
+    showSettingComponent() {
+      this.currentComponent = SettingComponent
+    },
+  },
+}
 </script>
 
 <template>
-  <div>
-    워크스페이스 인포
+  <div class="list">
+    <h1 class="overview">
+      overview
+    </h1>
+    <div class="workspaceHead">
+      <div class="image-container">
+        <img src="/person.png">
+      </div>
+      <div>my workspace</div>
+    </div>
+    <div class="workspaceHead">
+      <div class="overviewText" @click="showOverviewComponent">
+        overview
+      </div>
+      <div class="settingText" @click="showSettingComponent">
+        setting
+      </div>
+    </div>
+    <component :is="currentComponent" />
   </div>
 </template>
+
+<style scoped>
+.list{
+
+  border: 1px solid #000;
+  display: flex; /* 부모 요소를 플렉스 컨테이너로 설정 */
+  flex-direction: column; /* 아이템을 수직으로 정렬하기 위해 컬럼 방향 설정 */
+  /* align-items: left; 수평 가운데 정렬 */
+}
+.overview{
+  border-bottom: 1px solid #000; /* 밑줄 추가 */
+  height: fit-content
+
+}
+.workspaceHead {
+  padding-bottom: 10px;
+  display: flex; /* .workspaceHead를 플렉스 컨테이너로 설정 */
+  align-items: center; /* 수평 가운데 정렬 */
+  border-bottom: 1px solid #000; /* 밑줄 추가 */
+}
+
+.image-container {
+  margin-right: 10px; /* 이미지와 텍스트 사이의 간격을 조절 */
+}
+.overviewText{
+  margin-left: 10px; /* 이미지와 텍스트 사이의 간격을 조절 */
+
+}
+.settingText{
+  margin-left: 10px; /* 이미지와 텍스트 사이의 간격을 조절 */
+}
+</style>
