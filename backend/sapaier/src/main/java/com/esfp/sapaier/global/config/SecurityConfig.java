60984a057bd8d15b56de7ead4aade682bf496a33
tjdfkr011 <tjdfkr011@naver.com>
2023-11-08 14:19:45 +0900

    Feat : REVERT

--- backend/sapaier/src/main/java/com/esfp/sapaier/global/config/SecurityConfig.java
+++ backend/sapaier/src/main/java/com/esfp/sapaier/global/config/SecurityConfig.java
@@ -67,6 +67,7 @@ public class SecurityConfig {
 		httpSecurity
 			.authorizeHttpRequests(c -> {
 				c
+					.requestMatchers(HttpMethod.GET, LOGIN_URI_LIST).permitAll()//권한 필요없는 GET 허용 리스트 설정
 					.requestMatchers(HttpMethod.GET, SWAGGER_URI_LIST).permitAll()//권한 필요없는 GET 허용 리스트 설정
 					.requestMatchers("/**").hasAnyRole("USER")//권한 별 접근 URL 설정
 					.anyRequest().authenticated();
@@ -110,8 +111,8 @@ public class SecurityConfig {
 		// 커스텀필터
 		httpSecurity
 			.addFilterBefore(customFilterFactory.createJwtAuthenticationFilter(),
-				UsernamePasswordAuthenticationFilter.class)
-			.securityMatcher("/api/v1/**");
+				UsernamePasswordAuthenticationFilter.class);
+		// .securityMatcher("/api/v1/**");
 
 		return httpSecurity.build();
 	}
