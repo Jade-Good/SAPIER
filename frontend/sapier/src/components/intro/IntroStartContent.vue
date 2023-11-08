6ee73538b8804eb0f9f938c9fed295be735c9502
tjdfkr011 <tjdfkr011@naver.com>
2023-11-08 12:39:59 +0900

    Feat : Intro

--- frontend/sapier/src/components/intro/IntroStartContent.vue
+++ frontend/sapier/src/components/intro/IntroStartContent.vue
@@ -1,8 +1,11 @@
 <template>
   
-  <button>
-    <p>SAPIER</p>
-  </button>
+  <div class ="intro_text">
+    <span>Welcome To <span style="color:#658DC6">S</span></span><span style="color:#0F4C81">API</span><span style="color:#658DC6">ER</span>
+    <br/>
+    <span style="font-size: 2rem">Simple and Fast API Workspace</span>
+  </div>
+
   
 </template>
 
@@ -11,5 +14,11 @@
 </script>
 
 <style>
+.intro_text{
+  margin-left: 20%;
+  color: #000;
+  font-size: 5rem;
+  font-weight: 400;
+}
 
 </style>
