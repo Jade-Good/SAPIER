41d0644f040f7832b1204fbfbd9176743b09c474
WonjunChun <chunjh1103@naver.com>
2023-11-08 13:37:02 +0900

    Feat : DailyHistoryDto, WorkspaceHistoryDto 추가

---
+++ backend/sapaier/src/main/java/com/esfp/sapaier/domain/history/model/dto/DailyHistoryDto.java
@@ -0,0 +1,20 @@
+package com.esfp.sapaier.domain.history.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DailyHistoryDto {
+    private String uuid;
+    private List<WorkspaceHistoryDto> workspaceHistories;
+    private LocalDate date;
+
+}
