cd785c36b5d9e13ea71dde5cfd3d669280936b34
WonjunChun <chunjh1103@naver.com>
2023-11-08 13:37:54 +0900

    Feat : HistoryRepository 추가
    - findAllByOrderByCreatedTimeDesc 메서드 추가

---
+++ backend/sapaier/src/main/java/com/esfp/sapaier/domain/history/repository/HistoryRepository.java
@@ -0,0 +1,10 @@
+package com.esfp.sapaier.domain.history.repository;
+
+import com.esfp.sapaier.domain.history.repository.entity.HistoryEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface HistoryRepository extends MongoRepository<HistoryEntity, String> {
+    List<HistoryEntity> findAllByOrderByCreatedTimeDesc();
+}
