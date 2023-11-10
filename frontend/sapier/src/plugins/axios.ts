a6845a810c8371680d737d7070a0b85f614952f7
Soomin_Kang <kangsm423@gmail.com>
2023-11-10 16:15:24 +0900

    Fix/FE : cors & cookie

--- frontend/sapier/src/plugins/axios.ts
+++ frontend/sapier/src/plugins/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_URL}`,
+  baseURL: '',
   headers: {
     'Content-Type': 'application/json',
   },
