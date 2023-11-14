f418ef41fb0978be8c770623ce15dd7f3afaf6d6
WonjunChun <chunjh1103@naver.com>
2023-11-14 16:54:53 +0900

    Fix : ResponseDto 수정
    - statusCode, statusText, errorMsg, errorStackTrace 추가

--- backend/sapaier/src/main/java/com/esfp/sapaier/domain/history/model/dto/ResponseDto.java
+++ backend/sapaier/src/main/java/com/esfp/sapaier/domain/history/model/dto/ResponseDto.java
@@ -12,8 +12,11 @@ import java.util.HashMap;
 @NoArgsConstructor
 @Builder
 public class ResponseDto { //Response 정보를 저장해놓는 Dto
-    private String status; //상태코드
-    private int responseTime; //응답시간
+    private int statusCode; //상태코드
+    private String statusText;
+    private long responseTime; //응답시간
     private HashMap<String, String> header;
     private String body;
+    private String errorMsg;
+    private String errorStackTrace;
 }
