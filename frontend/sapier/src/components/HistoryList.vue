e57f784cb63775a8a3511e2b3b6baa5bd30c652d
WonjunChun <chunjh1103@naver.com>
2023-11-14 14:47:24 +0900

    Chore : History console log

--- frontend/sapier/src/components/HistoryList.vue
+++ frontend/sapier/src/components/HistoryList.vue
@@ -51,6 +51,7 @@ export default defineComponent({
         async function getHistoryList(){
             try{
                 const uuid = UserStore.uuid
+                console.log('uuid: ', uuid)
                 const response = await axios.get(`/api/v1/history?uuid=${uuid}`)
                 console.log(response.data)
                 HistoryListStore.historyList.value = response.data
